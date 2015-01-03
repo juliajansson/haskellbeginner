582716becbfb15ee6f39daa54a6c3185f8e1b922
Patrik Jansson <patrik.ja@gmail.com>
2015-01-03 23:52:39 +0100

    Classical recursive example

---
+++ Gloss/SkinnyTree.hs
@@ -0,0 +1,27 @@
+import Graphics.Gloss
+
+edge :: Float
+edge = 40
+
+trunk :: Picture
+trunk = line [(0, 0), (0, edge)]
+
+tree :: Int -> Picture
+tree 0 = trunk
+tree n = let  smallerTree = scale 0.8 0.8 (tree (n-1))
+         in Pictures [ trunk
+                     , onTrunk  $  rotate (-15) smallerTree
+                     , onTrunk  $  rotate ( 20) smallerTree
+                     ]           
+
+onTrunk :: Picture -> Picture
+onTrunk = Translate 0 edge
+
+picture :: Picture
+picture = tree 7
+
+myWindow :: Display
+myWindow = InWindow "Nice Window" (500, 500) (10, 10)
+
+main :: IO ()
+main = display myWindow white picture
--- Gloss/Test.hs
+++ Gloss/Test.hs
@@ -4,4 +4,3 @@ picture = pictures [text "Hello", circle 80]
 
 myWindow = InWindow "Nice Window" (200, 200) (10, 10)
 main = display myWindow white picture
- 
