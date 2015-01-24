0d8ab22bee69f093d84a163aec58ee5d80a5a48c
Julia Jansson <julia.jansson@gmail.com>
2015-01-24 12:49:19 +0100

    A little Welcoming!

---
+++ smallfunctions/welcoming.hs
@@ -0,0 +1,11 @@
+main = do
+  putStrLn "What is your name?"
+  name <- getLine
+  putStrLn ("Hello " ++ name ++ "!:) I'm Julia.  How old are you?")
+  age <- getLine
+  putStrLn ("Nice! " ++ age ++ ". " ++ expression (read age))
+
+
+expression age | age > 15 =  "You're older than me!"
+	       | age == 15 = "We're the same age!"
+	       | age < 15 = "You're younger than me!"
