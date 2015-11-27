f69dbe12d416cabfc5c3ba0e0babee7d71e033d1
Julia Jansson <julia.jansson@gmail.com>
2015-11-27 20:17:47 +0100

    4

---
+++ ProgOlymp/Onlinekval15.hs
@@ -0,0 +1,15 @@
+kolla:: Eq a=>[a]->Int
+kolla []=0
+kolla (x:xs)|length samma>1=1+kolla resten
+            |otherwise     =kolla resten
+  where samma =takeWhile (x==) (x:xs)
+        resten=dropWhile (x==) (x:xs)
+
+qsort:: Ord a=> [a]->[a]
+qsort []    =[]
+qsort [x]   =[x]
+qsort (x:xs)=mindre++(x:större)
+  where mindre= qsort (filter (x>) xs)
+        större= qsort (filter (x<=) xs)
+
+solution xs=kolla (qsort xs)
