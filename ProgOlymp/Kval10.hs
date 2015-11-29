53151779e07119d292819b7825a7cb393af7f0b4
Julia Jansson <julia.jansson@gmail.com>
2015-11-29 20:14:38 +0100

    4

--- ProgOlymp/Kval10.hs
+++ ProgOlymp/Kval10.hs
@@ -1,3 +1,16 @@
 cube:: Int -> [Int]
 cube 1=[]
-cube a=[b,c,d|a³==b³+c³+d³, 0<b, 0<c, 0<d, b<a, c<a, d<a]
+cube a=head [[b,c,d]|b<-[1..a], c<-[1..a], d<-[1..a], a^3==b^3+c^3+d^3]
+
+kub:: Int -> [[Int]]
+kub 0=[[]]
+kub s= concatMap (\t -> map (t:) (kub (s-t^3))) förstatalet
+  where förstatalet=reverse (takeWhile ((<=s).(^3)) [1..])
+
+kub2:: Int -> Int -> [[Int]]
+kub2 m 0=[[]]
+kub2 0 s=[]
+kub2 m s= concatMap (\t -> map (t:) (kub2 (m-1) (s-t^3))) förstatalet
+  where förstatalet=reverse (takeWhile ((<=s).(^3)) [1..])
+
+topp n = head (dropWhile ((<=1).length) (concatMap (\i -> kub2 i (n^3)) [1..]))
